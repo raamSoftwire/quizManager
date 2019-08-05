ed2525c05f21f05e39f596161818f0ed564f1596
Raam Chauhan <raam.chauhan@softwire.com>
2019-08-05 15:07:01 +0100

    Wire up Firestore DB

--- src/components/indexPage.tsx
+++ src/components/indexPage.tsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
-
+import { db} from '../firebase';
 export class IndexPage extends Component {
+
+  quizzesRef = db.collection('quizzes');
+
+  test() {
+    this.quizzesRef.get()
+      .then((querySnapshot) =>{
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+        });
+      })
+  };
+
   render() {
-    return <div>Index</div>
+    this.test();
+    return <div>
+      Index
+    </div>
   }
 }
 
